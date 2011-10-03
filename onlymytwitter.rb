a8208a7a827050afdfa6747bae0ed9852aa36267
Mitsuo NAKAJIMA <haroperitan@gmail.com>
2011-10-03 13:59:46 +0900

    show recent timeline

--- onlymytwitter.rb
+++ onlymytwitter.rb
@@ -10,11 +10,26 @@ class TweetDB
 	end
 
 	def update str
-		@db.execute("insert into tweet values(\"%s\", \"%s\", \"%s\");" % [@author, `date +%s`, str])
+		date = `date +%s`.chomp
+		@db.execute("insert into tweet values(\"%s\", \"%s\", \"%s\");" % [@author, date, str])
+	end
+
+	def timeline count = 10
+		recent_tweets = []
+		@db.execute("select * from tweet") do |row|
+			recent_tweets.push row.join("\t")
+			if recent_tweets.size > count
+				recent_tweets.shift
+			end
+		end
+		return recent_tweets
 	end
 end
 
 db = TweetDB.new
+
+db.timeline.each { |row| puts row }
+
 while l = gets
 	db.update l.chomp
 end
